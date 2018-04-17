58fd2b1a400a7e8b87556977af335853faf7d736
aurwin <dev@avqo.uk>
2018-04-18 02:36:27 +1000

    Record downloaded version

--- soe.sh
+++ soe.sh
@@ -18,18 +18,27 @@
 set -eo pipefail
 
 SOE_USER=$USER
-SOE_INSTALL=/home/$SOE_USER/.soe
+SOE_INSTALL_DIR=/home/$SOE_USER/.soe
 SOE_VERSION="1.0.0"
 
 SOE_URL="https://github.com/avqodev/ubuntu-soe/archive/master.zip"
-SOE_STAGE_DIRECTORY="${SOE_INSTALL}/stage"
-SOE_ZIP="${SOE_STAGE_DIRECTORY}/soe-${SOE_VERSION}.zip"
+SOE_STAGE_DIR="${SOE_INSTALL_DIR}/stage"
+SOE_ZIP="${SOE_STAGE_DIR}/soe-${SOE_VERSION}.zip"
 
 download() {
-    mkdir --parents $SOE_INSTALL
-    mkdir --parents $SOE_INSTALL/logs
-    mkdir --parents $SOE_INSTALL/temp
+    mkdir --parents $SOE_INSTALL_DIR
+    mkdir --parents $SOE_STAGE_DIR
+    mkdir --parents $SOE_INSTALL_DIR/logs
+    mkdir --parents $SOE_INSTALL_DIR/temp
     curl --location --progress-bar "${SOE_URL}" > "$SOE_ZIP"
-    # unzip -qo "$SOE_ZIP" -d "$SOE_STAGE_DIRECTORY"
+    unzip -qo "$SOE_ZIP" -d "$SOE_INSTALL_DIR"
+    mv "$SOE_STAGE_DIR"/* "$SOE_INSTALL_DIR"
 }
+
+set_version() {
+    mkdir --parents $SOE_INSTALL/var
+    echo "$SOE_VERSION" > "${SOE_INSTALL_DIR}/var/version"
+}
+
 download
+set_version
