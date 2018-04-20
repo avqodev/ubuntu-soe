8ba5b948ea30da1855edbf9e180ddafc6cecd802
aurwin <dev@avqo.uk>
2018-04-21 02:10:26 +1000

    Native java install instead of sdkman

--- bin/root/install-java.sh
+++ bin/root/install-java.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-add-apt-repository --yes ppa:webupd8team/java
-
-apt-get --assume-yes update
-
-apt-get --assume-yes install oracle-java8-installer
-apt-get --assume-yes install openjdk-8-jdk
-apt-get --assume-yes install default-jdk
-apt-get --assume-yes install oracle-java8-set-default
+java-install() {
+    add-apt-repository --yes ppa:webupd8team/java
+    
+    apt-get --assume-yes update
+    
+    apt-get --assume-yes install oracle-java8-installer
+    apt-get --assume-yes install openjdk-8-jdk
+    apt-get --assume-yes install default-jdk
+    apt-get --assume-yes install oracle-java8-set-default
+}
+java-install
--- bin/user/install-sdkman-packages.sh
+++ bin/user/install-sdkman-packages.sh
@@ -3,12 +3,8 @@
 source $SOE_INSTALL/bin/common/prop.sh
 
 sdkman-java-install() {
-    sdk install java $(prop 'java.8.version')-oracle
     sdk install java $(prop 'java.9.version')-openjdk
-    sdk install java $(prop 'java.9.version')-oracle
     sdk install java $(prop 'java.10.version')-openjdk
-    sdk install java $(prop 'java.10.version')-oracle
-    sdk default java $(prop 'java.8.version')-oracle
 }
 
 sdkman-maven-install() {
